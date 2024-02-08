1ab35c6d4b9e5efff5ec59d040e3972780937f5a
txngUI <tanguy.davidpro85@gmail.com>
2024-02-08 18:58:32 +0100

    Initial commit

---
+++ app/src/main/java/com/example/champscie/MinChamp.java
@@ -0,0 +1,14 @@
+package com.example.champscie;
+
+import java.util.LinkedList;
+
+public class MinChamp {
+    private String id;
+    private String name;
+    private String title;
+    private String partype;
+    private LinkedList<String> tags;
+    private String blurb;
+    private int key; // Order of apparition
+    private String image;
+}
